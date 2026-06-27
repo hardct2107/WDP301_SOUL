e0ff098c5d0aa8a70b3a0826ededdcc74f2199e5
nozero2306 <lethanhdat23062004@gmail.com>
2026-06-27 11:42:58 +0700

    deploy Vercel

--- frontend/APP/soul_ReactNative/src/api/config.ts
+++ frontend/APP/soul_ReactNative/src/api/config.ts
@@ -1,14 +1,13 @@
 import { Platform } from "react-native";
 
+const LOCAL_API_URL = Platform.select({
+  android: "http://192.168.2.43:5000/api",
+  ios: "http://localhost:5000/api",
+  web: "http://localhost:5000/api",
+  default: "http://localhost:5000/api",
+});
+
 export const API_BASE_URL =
-  process.env.EXPO_PUBLIC_API_URL ||
-  Platform.select({
-    //android: "http://10.0.2.2:5000/api",
-    android: "http://192.168.2.43:5000/api",
-    ios: "http://localhost:5000/api",
-    web: "http://localhost:5000/api",
-    default: "http://localhost:5000/api",
-  });
+  process.env.EXPO_PUBLIC_API_URL || LOCAL_API_URL;
 
 console.log("API_BASE_URL =", API_BASE_URL);
-
